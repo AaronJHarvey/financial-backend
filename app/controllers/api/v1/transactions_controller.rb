16872ded369ef1a39dcc5b4030e85cfe8b921432
AaronJHarvey <Harvey.AaronJ@gmail.com>
2021-05-11 17:03:09 -0400

    Sets up controllers, fixes routes, and updats account model

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,37 @@
 class Api::v1::TransactionsController < ApplicationController
-end
+  before_action :set_account
+
+  def index
+    @transactions = @account.transactions
+    render json: @transactions
+  end
+
+  def create
+    @transaction = @account.transactions.new(transaction_params)
+    if @account.update_balance(@transaction) != "Insufficient Funds"
+      @transaction.save
+      render json: @transaction
+    else
+      render json: {error: "Insufficient Funds"}
+    end
+  end
+
+  def show
+    @transaction = @account.transactions.find_by(id: params[:id])
+    render json: @transaction
+  end
+
+  def destroy
+  end
+
+  private
+
+    def set_account
+      @account = Account.find(params[:account_id])
+    end
+
+    def transaction_params
+      params.require(:transaction).permit(:amount, :account_id, :kind, :date, :description)
+    end
+
+  end
